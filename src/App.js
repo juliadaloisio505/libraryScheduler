c2a2699efd541873b3773a720c2cbeb540d4e817
Julia DAloisio <heydjulia@Julias-MacBook-Air.local>
2024-09-26 21:32:02 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Layout } from './Layout';
+import { Home } from './HomePage';
+import { Page1 } from './page1';
+import { Page2 } from './page2';
+import { Page4 } from './page4';
+
+
+
 
 function App() {
+
+  
+
   return (
+  
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+      <h1 className="heading">Welcome to Library Scheduler!</h1>
+      <h3 className="subheading">Let's plan your reading schedule...</h3>
+
+
+
+
+      
+    
+
+    <Router>
+      <Routes>
+
+        <Route element={<Layout/>}>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/page1" element={<Page1/>}/>
+        <Route path="/page2" element={<Page2/>}/>
+        <Route path="/page4" element={<Page4/>}/>
+        </Route>
+       
+      </Routes>
+    </Router>
+
+
+     
+      
     </div>
+
+    
+
   );
 }
 
